58d4dcf098b3858a9c38c1c0829205051d613aff
Shino <largythomas@gmail.com>
2026-03-11 23:13:16 +0100

    Indentation Review

--- Tetris/js/core/board.js
+++ Tetris/js/core/board.js
@@ -3,35 +3,35 @@ import { createLevels, updateLevel, levels } from '../game/level.js';
 import { updateCoins } from '../game/coins.js';
 
 export function createMatrix(w, h) {
-    const matrix = [];
-    while (h--) {
-        matrix.push(new Array(w).fill(0));
-    }
-    return matrix;
+	const matrix = [];
+	while (h--) {
+		matrix.push(new Array(w).fill(0));
+	}
+	return matrix;
 }
 
 export function merge(arena, player) {
-    player.matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                arena[y + player.pos.y][x + player.pos.x] = value;
-            }
-        });
-    });
+	player.matrix.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value !== 0) {
+				arena[y + player.pos.y][x + player.pos.x] = value;
+			}
+		});
+	});
 }
 
 export function collide(arena, player) {
-    const m = player.matrix;
-    const o = player.pos;
-    for (let y = 0; y < m.length; ++y) {
-        for (let x = 0; x < m[y].length; ++x) {
-            if (m[y][x] !== 0) {
-                if (!arena[y + o.y] || arena[y + o.y][x + o.x] !== 0) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
+	const m = player.matrix;
+	const o = player.pos;
+	for (let y = 0; y < m.length; ++y) {
+		for (let x = 0; x < m[y].length; ++x) {
+			if (m[y][x] !== 0) {
+				if (!arena[y + o.y] || arena[y + o.y][x + o.x] !== 0) {
+					return true;
+				}
+			}
+		}
+	}
+	return false;
 }
 
--- Tetris/js/ui/renderer.js
+++ Tetris/js/ui/renderer.js
@@ -1,20 +1,20 @@
 import { canvas, context, arena, player, keys_colors } from '../data/config.js';
 
 export function drawMatrix(matrix, offset) {
-    matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                context.fillStyle = keys_colors[value];
-                context.fillRect(x + offset.x, y + offset.y, 1, 1);
-            }
-        });
-    });
+	matrix.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value !== 0) {
+				context.fillStyle = keys_colors[value];
+				context.fillRect(x + offset.x, y + offset.y, 1, 1);
+			}
+		});
+	});
 }
 
 export function draw() {
-    context.fillStyle = '#fff';
-    context.fillRect(0, 0, canvas.width / 20, canvas.height / 20);
+	context.fillStyle = '#fff';
+	context.fillRect(0, 0, canvas.width / 20, canvas.height / 20);
 
-    drawMatrix(arena, {x: 0, y: 0});
-    drawMatrix(player.matrix, player.pos);
+	drawMatrix(arena, {x: 0, y: 0});
+	drawMatrix(player.matrix, player.pos);
 }
